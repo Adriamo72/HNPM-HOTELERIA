f7402f2116eea5a04726980e470e61550f951b2f
Adriamo72 <capitanmorenoadrian@gmail.com>
2026-02-21 05:13:02 -0300

    CAMBIO APP

--- src/App.js
+++ src/App.js
@@ -5,17 +5,11 @@ import FormularioPiso from './components/FormularioPiso';
 function App() {
   const [pañoleroLogueado, setPañoleroLogueado] = useState(null);
 
-  // Función para cerrar sesión si se desea más tarde
-  const cerrarSesion = () => setPañoleroLogueado(null);
-
   return (
     <div className="App bg-slate-950 min-h-screen">
       {!pañoleroLogueado ? (
-        // Si no hay nadie logueado, mostramos el Login
         <Login alLoguear={(dni) => setPañoleroLogueado(dni)} />
       ) : (
-        // Si ya ingresó el DNI, mostramos el formulario del piso
-        // Aquí es donde el QR en el futuro pasaría el "nroPiso"
         <FormularioPiso 
           nroPiso={1} 
           dniPañolero={pañoleroLogueado} 
